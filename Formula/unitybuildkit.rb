e7779f899a8101610b3ea0d56cba45c3e437c23b
Eric Miller <eric@handsome.is>
2017-10-23 17:41:52 -0500

    Update Homebrew Formula

--- Formula/unitybuildkit.rb
+++ Formula/unitybuildkit.rb
@@ -2,7 +2,7 @@ class UnityBuildKit < Formula
     desc "A command line tool that generates an iOS application with an embedded Unity scene"
     homepage "https://github.com/handsomecode/UnityBuildKit"
     url "https://github.com/handsomecode/UnityBuildKit/archive/0.8.0.tar.gz"
-    sha256 "a5041d758130924c92c7c67ec1447fc79de0f28c29dd7437836a676c6501561f"
+    sha256 "80ae226bcd7afbe904bf30f2b28deac8b560f88ccac35d46b12f1278304cdddb"
     head "https://github.com/handsomecode/UnityBuildKit.git"
 
     depends_on :xcode
